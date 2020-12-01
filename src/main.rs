b09e5257d8650849c1ffa674dc94fa21427579d3
Ben Rog-Wilhelm <zorba-githubdesktop20180420@pavlovian.net>
2020-12-01 01:36:39 -0600

    1.1 complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+
+use std::io;
+
+fn read_numbers() -> Vec<i32> {
+    let mut rv: Vec<i32> = Vec::new();
+
+    let mut input = String::new();
+    loop {
+        match io::stdin().read_line(&mut input) {
+            Ok(0) => return rv,
+            Ok(_) => {
+                input = input.trim().to_string();
+                println!("{:#?}", input);
+                rv.push(input.parse::<i32>().unwrap());
+            },
+            Err(_) => return rv,
+        }
+        input.clear();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let numbers = read_numbers();
+    for x in &numbers {
+        for y in &numbers {
+            if x + y == 2020 {
+                println!("{} {}", x, y);
+            }
+        }
+    }
 }

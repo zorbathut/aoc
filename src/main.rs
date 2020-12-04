f632a5ebbd0ebdad7f21dbb84f44f8ee72dc5465
Ben Rog-Wilhelm <zorba-githubdesktop20180420@pavlovian.net>
2020-12-03 23:26:03 -0600

    4.2 complete (forgot to check in 4.1, sorry)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 
 use std::io;
 use regex::Regex;
+use std::collections::HashMap;
+
+#[macro_use] extern crate scan_fmt;
 
 fn read_numbers() -> Vec<i32> {
     let mut rv: Vec<i32> = Vec::new();
@@ -29,7 +32,6 @@ fn read_lines() -> Vec<String> {
             Ok(0) => return rv,
             Ok(_) => {
                 input = input.trim().to_string();
-                println!("{:#?}", input);
                 rv.push(input);
             },
             Err(_) => return rv,
@@ -40,29 +42,83 @@ fn read_lines() -> Vec<String> {
 fn main() {
     let lines = read_lines();
 
-    let mut dx: [usize; 5] = [1, 3, 5, 7, 1];
-    let mut dy: [usize; 5] = [1, 1, 1, 1, 2];
+    let re = Regex::new(r"(?P<key>[a-z]+):(?P<value>[^ ]+)").unwrap();
 
-    let mut accum: i64 = 1;
+    let reYears = Regex::new(r"^[\d]{4}$").unwrap();
+    let reHeight = Regex::new(r"^(?P<num>\d+)(?P<value>in|cm)").unwrap();
+    let reColor = Regex::new(r"^#[0-9a-f]{6}$").unwrap();
+    let reEye = Regex::new(r"^amb|blu|brn|gry|grn|hzl|oth$").unwrap();
+    let rePid = Regex::new(r"^[\d]{9}$").unwrap();
 
-    for i in 0..dx.len() {
-        let mut x = 0;
-        let mut y = 0;
+    let mut keys = HashMap::new();
+    let mut count = 0;
 
-        let mut trees = 0;
+    for line in lines {
+        println!("{:#?}", line);
 
-        while y < lines.len() {
-            let spot = lines[y].bytes().nth(x % lines[y].len()).unwrap();
-            if spot == '#' as u8 {
-                trees += 1;
+        if re.is_match(&line) {
+            for capture in re.captures_iter(&line) {
+                println!("{:#?}", capture);
+
+                keys.insert(capture.name("key").unwrap().as_str().to_string(), capture.name("value").unwrap().as_str().to_string());
+            }
+        } else {
+            println!("FIN");
+
+            if keys.contains_key("byr") && keys.contains_key("iyr") && keys.contains_key("eyr") && keys.contains_key("hgt") && keys.contains_key("hcl") && keys.contains_key("ecl") && keys.contains_key("pid") {
+                let mut valid = true;
+                if !reYears.is_match(&keys["byr"]) {
+                    valid = false;
+                }
+                if !reYears.is_match(&keys["iyr"]) {
+                    valid = false;
+                }
+                if !reYears.is_match(&keys["eyr"]) {
+                    valid = false;
+                }
+                if !reHeight.is_match(&keys["hgt"]) {
+                    valid = false;
+                }
+                if !reColor.is_match(&keys["hcl"]) {
+                    valid = false;
+                }
+                if !reEye.is_match(&keys["ecl"]) {
+                    valid = false;
+                }
+                if !rePid.is_match(&keys["pid"]) {
+                    valid = false;
+                }
+
+                if valid {
+                    let byr = keys["byr"].parse::<i32>().unwrap();
+                    let iyr = keys["iyr"].parse::<i32>().unwrap();
+                    let eyr = keys["eyr"].parse::<i32>().unwrap();
+
+                    if byr < 1920 || byr > 2002 || iyr < 2010 || iyr > 2020 || eyr < 2020 || eyr > 2030 {
+                        valid = false;
+                    }
+
+                    let hgt = reHeight.captures(&keys["hgt"]).unwrap();
+                    let hgam = hgt.name("num").unwrap().as_str().parse::<i32>().unwrap();
+                    let hgun = hgt.name("value").unwrap().as_str();
+
+                    if hgun == "cm" && (hgam < 150 || hgam > 193) {
+                        valid = false;
+                    }
+                    if hgun == "in" && (hgam < 59 || hgam > 76) {
+                        valid = false;
+                    }
+
+                    if valid {
+                        count += 1;
+                        println!("VALID");    
+                    }
+                }
             }
 
-            x += dx[i];
-            y += dy[i];
+            keys.clear();
         }
-
-        accum *= trees;
     }
-    
-    dbg!(accum);
+
+    println!("{:#?}", count);
 }

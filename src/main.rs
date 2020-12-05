bc006a45e13b36f4e2ea0cab7ed15e5e63f4287c
Ben Rog-Wilhelm <zorba-githubdesktop20180420@pavlovian.net>
2020-12-04 23:19:21 -0600

    5.1 and 5.2 complete

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 
 use std::io;
+use std::cmp;
 use regex::Regex;
+use std::collections::HashSet;
 use std::collections::HashMap;
 
 #[macro_use] extern crate scan_fmt;
@@ -42,83 +44,32 @@ fn read_lines() -> Vec<String> {
 fn main() {
     let lines = read_lines();
 
-    let re = Regex::new(r"(?P<key>[a-z]+):(?P<value>[^ ]+)").unwrap();
+    let mut best = 0;
 
-    let reYears = Regex::new(r"^[\d]{4}$").unwrap();
-    let reHeight = Regex::new(r"^(?P<num>\d+)(?P<value>in|cm)").unwrap();
-    let reColor = Regex::new(r"^#[0-9a-f]{6}$").unwrap();
-    let reEye = Regex::new(r"^amb|blu|brn|gry|grn|hzl|oth$").unwrap();
-    let rePid = Regex::new(r"^[\d]{9}$").unwrap();
-
-    let mut keys = HashMap::new();
-    let mut count = 0;
+    let mut seats = HashSet::new();
 
     for line in lines {
         println!("{:#?}", line);
 
-        if re.is_match(&line) {
-            for capture in re.captures_iter(&line) {
-                println!("{:#?}", capture);
+        let mut pos = 0;
 
-                keys.insert(capture.name("key").unwrap().as_str().to_string(), capture.name("value").unwrap().as_str().to_string());
+        for kar in line.chars() {
+            pos <<= 1;
+
+            if kar == 'B' || kar == 'R' {
+                pos += 1;
             }
-        } else {
-            println!("FIN");
+        }
 
-            if keys.contains_key("byr") && keys.contains_key("iyr") && keys.contains_key("eyr") && keys.contains_key("hgt") && keys.contains_key("hcl") && keys.contains_key("ecl") && keys.contains_key("pid") {
-                let mut valid = true;
-                if !reYears.is_match(&keys["byr"]) {
-                    valid = false;
-                }
-                if !reYears.is_match(&keys["iyr"]) {
-                    valid = false;
-                }
-                if !reYears.is_match(&keys["eyr"]) {
-                    valid = false;
-                }
-                if !reHeight.is_match(&keys["hgt"]) {
-                    valid = false;
-                }
-                if !reColor.is_match(&keys["hcl"]) {
-                    valid = false;
-                }
-                if !reEye.is_match(&keys["ecl"]) {
-                    valid = false;
-                }
-                if !rePid.is_match(&keys["pid"]) {
-                    valid = false;
-                }
+        best = cmp::max(best, pos);
+        seats.insert(pos);
+    }
 
-                if valid {
-                    let byr = keys["byr"].parse::<i32>().unwrap();
-                    let iyr = keys["iyr"].parse::<i32>().unwrap();
-                    let eyr = keys["eyr"].parse::<i32>().unwrap();
-
-                    if byr < 1920 || byr > 2002 || iyr < 2010 || iyr > 2020 || eyr < 2020 || eyr > 2030 {
-                        valid = false;
-                    }
-
-                    let hgt = reHeight.captures(&keys["hgt"]).unwrap();
-                    let hgam = hgt.name("num").unwrap().as_str().parse::<i32>().unwrap();
-                    let hgun = hgt.name("value").unwrap().as_str();
-
-                    if hgun == "cm" && (hgam < 150 || hgam > 193) {
-                        valid = false;
-                    }
-                    if hgun == "in" && (hgam < 59 || hgam > 76) {
-                        valid = false;
-                    }
-
-                    if valid {
-                        count += 1;
-                        println!("VALID");    
-                    }
-                }
-            }
-
-            keys.clear();
+    for i in 1..2000 {
+        if seats.contains(&(i - 1)) && seats.contains(&(i + 1)) && !seats.contains(&i) {
+            println!("{}", i);
         }
     }
 
-    println!("{:#?}", count);
+    println!("{:#?}", best);
 }

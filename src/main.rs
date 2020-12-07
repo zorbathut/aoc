a36f00f5363eb939d20d6c0113e81d3b63cacc71
Ben Rog-Wilhelm <zorba-githubdesktop20180420@pavlovian.net>
2020-12-06 23:29:09 -0600

    7.1 complete

--- src/main.rs
+++ src/main.rs
@@ -41,37 +41,88 @@ fn read_lines() -> Vec<String> {
     }
 }
 
+fn eval(bags: &HashMap<String, HashSet<String>>, bag: &str, evaluated: &mut HashSet<String>, valid: &mut HashSet<String>)
+{
+    if evaluated.contains(bag) {
+        return
+    }
+
+    evaluated.insert(bag.to_string());
+
+    let mut victory = false;
+
+    for child in &bags[bag] {
+        eval(bags, &child, evaluated, valid);
+        if valid.contains(child) {
+            victory = true;
+        }
+    }
+
+    if victory {
+        valid.insert(bag.to_string());
+    }
+}
+
+fn eval2(bags: &HashMap<String, HashSet<String>>, bag: &str, evaluated: &mut HashSet<String>, valid: &mut HashSet<String>)
+{
+    if evaluated.contains(bag) {
+        return
+    }
+
+    evaluated.insert(bag.to_string());
+    valid.insert(bag.to_string());
+
+    if bags.contains_key(bag) {
+        for child in &bags[bag] {
+            eval2(bags, child, evaluated, valid);
+        }
+    }
+}
+
 fn main() {
     let lines = read_lines();
 
-    let mut best = 0;
-
-    let mut first = true;
-    let mut seats: HashSet<char> = HashSet::new();
+    let mut bags: HashMap<String, HashSet<String>> = HashMap::new();
+    let mut bagContain: HashMap<String, HashSet<String>> = HashMap::new();
+    let re = Regex::new(r"(?P<count>[0-9]+) (?P<bagtype>[^ ]+ [^ ]+) bag").unwrap();
 
     for line in lines {
         println!("{:#?}", line);
 
-        if line.len() == 0 {
-            best += seats.len();
-            println!("ADD: {:#?}", seats.len());
-            seats.clear();
-            first = true;
-        } else if first {
-            for kar in line.chars() {
-                seats.insert(kar);
-            }
-            first = false;
-        } else {
-            let mut seatsb: HashSet<char> = HashSet::new();
-            for kar in line.chars() {
-                seatsb.insert(kar);
-            }
+        let bagname = (line.split(" bags contain ").collect::<Vec<&str>>())[0];
+        dbg!(bagname);
 
-            seats = seats.intersection(&seatsb).copied().collect();
-            println!("SEE: {:#?}", seats);
+        let mut hs: HashSet<String> = HashSet::new();
+
+        if !line.contains("no other bags") {
+            for capture in re.captures_iter(&line) {
+                let childname = capture.name("bagtype").unwrap().as_str().to_string();
+                dbg!(&childname);
+                hs.insert(childname.to_string());
+
+                if !bagContain.contains_key(&childname) {
+                    bagContain.insert(childname.to_string(), HashSet::new());
+                }
+                bagContain.get_mut(&childname).unwrap().insert(bagname.to_string());
+            }
         }
+
+        bags.insert(bagname.to_string(), hs);
     }
 
-    println!("{:#?}", best);
+    let mut evaluated = HashSet::new();
+    let mut valid = HashSet::new();
+
+    eval2(&bagContain, "shiny gold", &mut evaluated, &mut valid);
+
+    /*let mut evaluated = HashSet::new();
+    let mut valid = HashSet::new();
+    evaluated.insert("shiny gold".to_string());
+    valid.insert("shiny gold".to_string());
+    for bag in &bags {
+        eval(&bags, &bag.0, &mut evaluated, &mut valid);
+    }*/
+    dbg!(&valid);
+
+    dbg!(valid.len() - 1);
 }
